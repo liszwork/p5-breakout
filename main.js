8cfca59b6f0e4f7d5ec91c700b717892eda90f63
lis <zz.iz.0pnup@gmail.com>
2022-06-16 23:01:27 +0900

    ブロックの配列化

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
 // FPS
 const FPS = 1 / 60;
 
-
-function setup() {
-  createCanvas(400, 400);
-}
-
 class Vec2 {
   /**
    * ベクトル
@@ -71,6 +66,11 @@ class Vec2 {
 }
 
 class Ball {
+  /**
+   * @param {Vec2} _p ボールの速度ベクトル成分
+   * @param {Vec2} _v ボールの速度ベクトル成分
+   * @param {number} _r ボールの半径
+   */
   constructor(_p, _v, _r) {
     this.p = _p;  // ボールの速度ベクトル成分
     this.v = _v;  // ボールの速度ベクトル成分
@@ -79,6 +79,10 @@ class Ball {
 }
 
 class Block {
+  /**
+   * @param {Vec2} _p ブロックの中心位置ベクトル
+   * @param {number} _r ブロックの半径
+   */
   constructor(_p, _r) {
     this.p = _p;  // ブロックの中心位置ベクトル
     this.r = _r;  // ブロックの半径
@@ -86,10 +90,22 @@ class Block {
 }
 
 // ボール
-const ball = new Ball(new Vec2(100, 150), new Vec2(120, 60), 20);
-const block = new Block(new Vec2(200, 150), 80);
+const ball = new Ball(new Vec2(100, 40), new Vec2(120, 60), 10);
+const blocks = [];
+const BLOCK_SPACE = 50;
 // 点数
 let score = 0;
+
+function setup() {
+  createCanvas(400, 400);
+  // ブロックを生成
+  for (let i = 0; i < 12; i++) {
+    let x = 90 * (i % 4) + BLOCK_SPACE;
+    let y = 50 * floor(i / 4) + BLOCK_SPACE;
+    let p = new Vec2(x, y);
+    blocks.push(new Block(p, 30));
+  }
+}
 
 function draw() {
 
@@ -108,22 +124,29 @@ function draw() {
     score++;
   }
   // ボールとブロックの衝突判定
-  let d = block.p.sub(ball.p).mag();    // 距離
-  if (d < (ball.r + block.r)) {
-    // ぶつかっていたらボールの速度を反射
-    let v = ball.v;
-    let w = ball.p.sub(block.p);
-    let cosTheta = v.mul(-1).dot(w) / (v.mul(-1).mag() * w.mag());
-    let n = w.norm().mul(v.mag()*cosTheta);
-    let r = v.add(n.mul(2));
-    ball.v = r;
+  for (const block of blocks) {
+    let d = block.p.sub(ball.p).mag();    // 距離
+    if (d < (ball.r + block.r)) {
+      // ぶつかっていたらボールの速度を反射
+      let v = ball.v;
+      let w = ball.p.sub(block.p);
+      let cosTheta = v.mul(-1).dot(w) / (v.mul(-1).mag() * w.mag());
+      let n = w.norm().mul(v.mag() * cosTheta);
+      let r = v.add(n.mul(2));
+      ball.v = r;
+    }
   }
-
 
   background(220);
   fill('#777');
+  stroke('#555');
   circle(ball.p.x, ball.p.y, 2 * ball.r); // ボールを描画
-  circle(block.p.x, block.p.y, 2 * block.r); // ブロックを描画
+
+  fill('#00a')
+  stroke('#bbb')
+  for (const block of blocks) {
+    circle(block.p.x, block.p.y, 2 * block.r); // ブロックを描画
+  }
 
   text(score, 0, 0, 100, 100);
 }
